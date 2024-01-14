7ee886ff15b68f648595087f9f3b8e9472ba6081
Pipeara <piposo3@hotmail.es>
2024-01-14 00:14:07 -0300

    modificacion index

---
+++ consultas.js
@@ -0,0 +1,76 @@
+const format = require('pg-format');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    password: 'tu_contraseña',
+    database: 'joyas',
+    allowExitOnIdle: true
+});
+
+const obtenerJoyas = async ({ limits = 10, order_by = "id_ASC", page = 1 }) => {
+    const offset = (page - 1) * limits;
+    const [campo, direccion] = order_by.split("_");
+    const formattedQuery = format('SELECT * FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s', campo, direccion, limits, offset);
+    pool.query(formattedQuery);
+    const { rows: joyas } = await pool.query(formattedQuery);
+    return joyas;
+}
+
+const prepararHATEOAS = (joyas) => {
+    const results = joyas.map((j) => {
+        return {
+            name: j.nombre,
+            href: `/joyas/joya/${j.id}`,
+        }
+    });
+
+    const totalJoyas = joyas.length;
+
+    let stockTotal = 0;
+
+    for (let joya of joyas) {
+        stockTotal += joya.stock;
+    }
+
+    const HATEOAS = {
+        totalJoyas,
+        stockTotal,
+        results
+    }
+    return HATEOAS;
+}
+
+const ObtenerJoyasPorFiltros = async ({ precio_max, precio_min, categoria, metal }) => {
+    const filtros = [];
+    const values = [];
+
+    const agregarFiltro = (campo, comparador, valor) => {
+        values.push(valor);
+        filtros.push(`${campo} ${comparador} $${values.length}`);
+    }
+
+    if (precio_max) agregarFiltro('precio', '<=', precio_max);
+    if (precio_min) agregarFiltro('precio', '>=', precio_min);
+    if (categoria) agregarFiltro('categoria', '=', categoria);
+    if (metal) agregarFiltro('metal', '=', metal);
+
+    let consulta = "SELECT * FROM inventario";
+
+    if (filtros.length > 0) {
+        consulta += ` WHERE ${filtros.join(" AND ")}`;
+    }
+
+    const { rows: inventario } = await pool.query(consulta, values);
+
+    return inventario;
+}
+
+const ObtenerJoya = async (id) => {
+    const consulta = 'SELECT * FROM inventario WHERE id = $1';
+    const result = await pool.query(consulta, [id]);
+    return result;
+}
+
+module.exports = { obtenerJoyas, prepararHATEOAS, ObtenerJoyasPorFiltros, ObtenerJoya };
---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const { obtenerJoyas, obtenerJoyasFiltradas } = require('./consultas.js');
+const cors = require('cors');
+const { queryReport, contadorConsultas } = require('./queryReport'); // Importar los middlewares
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+// Aplicar los middlewares a todas las rutas
+app.use(queryReport);
+app.use(contadorConsultas);
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
+// Ruta para obtener todas las joyas con estructura HATEOAS
+app.get("/joyas", async (req, res) => {
+    try {
+        // Obtener parámetros de la query string
+        const { limits, page, order_by } = req.query;
+
+        // Obtener joyas según los parámetros
+        const joyas = await obtenerJoyas({ limits, page, order_by });
+
+        // Estructura HATEOAS
+        const joyasHATEOAS = joyas.map(joya => {
+            return {
+                id: joya.id,
+                nombre: joya.nombre,
+                descripcion: joya.descripcion,
+                _links: {
+                    self: { href: `/joyas/${joya.id}`, method: 'GET' },
+                }
+            };
+        });
+
+        res.json(joyasHATEOAS);
+    } catch (error) {
+        console.error('Error al obtener joyas', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
+
+app.get("/joyas/filtros", async (req, res) => {
+    try {
+        // Obtener parámetros de la query string
+        const { precio_max, precio_min, categoria, metal } = req.query;
+
+        // Obtener joyas filtradas según los parámetros
+        const joyasFiltradas = await obtenerJoyasFiltradas({ precio_max, precio_min, categoria, metal });
+
+        // Estructura HATEOAS para las joyas filtradas
+        const joyasFiltradasHATEOAS = joyasFiltradas.map(joya => {
+            return {
+                id: joya.id,
+                nombre: joya.nombre,
+                descripcion: joya.descripcion,
+                _links: {
+                    self: { href: `/joyas/${joya.id}`, method: 'GET' },
+                }
+            };
+        });
+
+        res.json(joyasFiltradasHATEOAS);
+    } catch (error) {
+        console.error('Error al obtener joyas filtradas', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
